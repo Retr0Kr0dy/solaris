b930e3de78fe4b3fbefbb193c524fa7d19131dc2
Rick Astley <never.gonna@giveu.up>
2023-09-05 16:01:28 +0200

    creating moving_dots() test.

--- source/libs/screen.h
+++ source/libs/screen.h
@@ -35,12 +35,49 @@ char **get_background() {
     return table;
 }
 
+static int x = 2;
+static int y = 2;
+static char charc = 'O';
+
+void **moving_dot()
+{
+    static void *elem[3];
+    elem[0] = &x;
+    elem[1] = &y;
+    elem[2] = &charc;
+
+    if (x < LENGTH - 2)
+    {
+        if (y >= HEIGHT - 2)
+        {
+            y = 2;
+        }
+        x+=3;
+    }
+    else
+    {
+        x = 2;
+        y++;
+    }
+
+    return elem;
+}
+
 char **get_elements() {
+    void **temp = moving_dot();
+    void *elem[3] = {*(int **) temp, *(int **) (temp + 1), *(char **) (temp + 2)};
+
+    int x_pos = *(int*) elem[0];
+    int y_pos = *(int*) elem[1];
+    char chr = *(char*) elem[2];
+
     for (int i = 0; i < HEIGHT; i++) {
         table[i] = (char *)malloc(LENGTH * sizeof(char));
         for (int j = 0; j < LENGTH; j++) {
             if (i == 0 || j == 0 || i == HEIGHT - 1 || j == LENGTH - 1) {
                 table[i][j] = '#';
+            } else if (i == y_pos && j == x_pos) {
+                table[i][j] = chr;
             } else {
                 table[i][j] = ' ';
             }
