f693dd6c21b8d33ebafab3ecbdbb62ba6359bd04
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2023-09-05 09:20:19 +0200

    Create screen.h

---
+++ source/libs/screen.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define HEIGHT 30
+#define LENGTH 60
+
+char *table[HEIGHT]; 
+
+void show(char **table) {
+    system("clear");
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < LENGTH; j++) {
+            printf ("%c", table[i][j]);
+        }
+        printf ("\n");
+    }
+}
+
+char **get_background() {
+    for (int i = 0; i < HEIGHT; i++) {
+        table[i] = (char *)malloc(LENGTH * sizeof(char));
+        for (int j = 0; j < LENGTH; j++) {
+            if (i == 0 || j == 0 || i == HEIGHT - 1 || j == LENGTH - 1) {
+                table[i][j] = '#';
+            } else {
+                table[i][j] = ' ';
+            }
+        }
+    }
+
+    return table;
+}
