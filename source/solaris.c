6ae86b46eac63fa882f5509fe59412a652dc915b
Rick Astley <never.gonna@giveu.up>
2023-09-05 10:40:11 +0200

    Clock handling, say no to usleep()

--- source/solaris.c
+++ source/solaris.c
@@ -1,10 +1,11 @@
-#include "lib/screen.h"
+#include "libs/screen.h"
 #include <time.h>
 #include <string.h>
 
 int main(int argc, char *argv[])
 {
     int DEBUG = 0;
+    int FREQUENCY = 33;
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--debug") == 0) {
@@ -12,32 +13,45 @@ int main(int argc, char *argv[])
         }
     }
 
-
-
+    struct timespec start_time;
+    struct timespec from_start;
+    struct timespec current_time;
+    int ips = 0;
     int ifs = 0;
-    int ips;
-    int sec = 0;
-    clock_t t1 = clock();
+    int elapsed_time = 0;
+    int from_start_time = 0;
 
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+    current_time = start_time;
+
+    clock_gettime(CLOCK_MONOTONIC, &from_start);
+    current_time = from_start;
 
     while (1)
     {
-        char **bkgd = get_background();
-        show(bkgd);
-        //DEBUG
-        if (DEBUG) {
+        clock_gettime(CLOCK_MONOTONIC, &current_time);
+
+        from_start_time= (current_time.tv_sec - from_start.tv_sec) * 1000 +
+                       (current_time.tv_nsec - from_start.tv_nsec) / 1000000;
+
+        elapsed_time = (current_time.tv_sec - start_time.tv_sec) * 1000 +
+                       (current_time.tv_nsec - start_time.tv_nsec) / 1000000;
+
+        if (elapsed_time >= FREQUENCY)
+        {
+            char **bkgd = get_background();
+            show(bkgd);
+// DEBUG
+if (DEBUG) {
+    if ((from_start_time / 1000) != 0) {
+        ips = ifs / (from_start_time / 1000);
+    }
+    printf("time = %4d\tifs = %4d\tips = %4d\n", (from_start_time / 1000), ifs, ips);
+}
+// END DEBUG
+            start_time = current_time;
             ifs++;
-            clock_t t2 = clock() - t1;
-            sec = t2 * 100 / CLOCKS_PER_SEC;
-            if (sec != 0) {
-                ips = ifs / sec;
-            } else {
-                ips = 0;
-            }
-            printf("time = %4d\tifs = %4d\tips = %4d\n", sec, ifs, ips);
         }
-        //ENDUG
-        usleep(66000);
     }
 
     return 0;
